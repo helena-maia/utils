edbea93562884b8a3a0d2c50a3df7f993289602c
helena-maia <30292801+helena-maia@users.noreply.github.com>
2019-03-20 18:04:24 -0300

    Update remove_empty.py

--- download/remove_empty.py
+++ download/remove_empty.py
@@ -4,5 +4,6 @@ import sys
 videos = os.listdir(sys.argv[1])
 
 for v in videos:
-    if os.stat(sys.argv[1]+v).st_size == 0:
-        os.remove(sys.argv[1]+v)
+    path = os.path.join(sys.argv[1],v)
+    if os.stat(path).st_size == 0:
+        os.remove(path)
